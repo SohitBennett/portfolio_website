8b9371c1f6f8d8ddfed70556e55fb45edac9c0f3
SohitBennett <E22CSEU1409@bennett.edu.in>
2024-09-22 00:34:52 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import logo from "../assets/kevinRushLogo.png";
+import logo2 from "../assets/SohitJoshiLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            <img className="mx-2 w-16" src={logo2} alt="logo" />
+        </div>  
+        {/* <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <FaLinkedin />
+            <FaGithub />
+            <FaTwitter />
+            <FaInstagram />
+        </div>   */}
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <a href="https://www.linkedin.com/in/sohit-joshi-36b680235/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin />
+            </a>
+            <a href="https://github.com/SohitBennett" target="_blank" rel="noopener noreferrer">
+                <FaGithub />
+            </a>
+            <a href="https://x.com/Sohitjoshi21" target="_blank" rel="noopener noreferrer">
+                <FaTwitter />
+            </a>
+            <a href="https://www.instagram.com/sohitjsh" target="_blank" rel="noopener noreferrer">
+                <FaInstagram />
+            </a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
+
+
---
+++ src/constants/index.js
@@ -0,0 +1,78 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const ABOUT_TEXT2 = 'I am a Fullstack developer currently 3rd year in college persuing B.Tech in CSE, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects. ';
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "BookShelf",
+    image: project1,
+    description:
+      "A fully functional book browsing & tracking website with features like ai recommendation, personalised book selection, Pomodoro timer & user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "AI powered Gesture based Application Launcher",
+    image: project2,
+    description:
+      "An ai model based software for launching application on pc using an air canvas to simply just write the name of application to launch it.",
+    technologies: ["Python", "MeidaPipe", "OpenCV", "Tenserflow", "Windows API", "OCR model"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React", "Tailwind", "Framer-motion"],
+  },
+  {
+    title: "Parakh Practice Exam portal",
+    image: project4,
+    description:
+      "A platform for adaptive learning for students using AI model to set difficulty of pratice quizes adaptively. ",
+    technologies: ["HTML", "CSS", "Python", "django", "mySQL", "Random Forest"],
+  },
+];
+
+export const CONTACT = {
+  address: "Bennett University, Greater Noida, UP",
+  phoneNo: "+91 7302167270",
+  email: "sohitjoshi21@gmail.com",
+};
